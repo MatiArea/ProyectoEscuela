53e3622e51d46fadb0069508f842cf8717d90f4f
ivanchiabrera <44534420+ivanchiabrera@users.noreply.github.com>
2019-02-21 01:12:09 -0300

    fin

--- proyectoEscuela-FRONT/src/app/pages/admin/admin.module.ts
+++ proyectoEscuela-FRONT/src/app/pages/admin/admin.module.ts
@@ -3,21 +3,13 @@ import { NgModule } from '@angular/core';
 import { LoginGuard } from 'src/app/guards/login.guard';
 import { UnloginGuard } from 'src/app/guards/unlogin.guard';
 import { AppComponent } from 'src/app/app.component';
-import { AltausuarioComponent } from './altausuario/altausuario.component';
 import { EnviarnotificacionComponent } from './enviarnotificacion/enviarnotificacion.component';
-import { ListadoaniosComponent } from './listadoanios/listadoanios.component';
 import { ListadomateriasComponent } from './listadomaterias/listadomaterias.component';
-import { NotasComponent } from './notas/notas.component';
 import { PaneladminComponent } from './paneladmin/paneladmin.component';
 import { ADMIN_ROUTES } from './admin.routes';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CrearevaluacionComponent } from './crearevaluacion/crearevaluacion.component';
-import { NotaComponent } from './nota/nota.component';
-import { CargarevaluacionComponent } from './cargarevaluacion/cargarevaluacion.component';
-import { CargadenotasComponent } from './cargadenotas/cargadenotas.component';
-import { ListadodivComponent } from './listadodiv/listadodiv.component';
 import { ListadoalumnosComponent } from './listadoalumnos/listadoalumnos.component';
-import { MateriasalumnoComponent } from './materiasalumno/materiasalumno.component';
 import { FormsModule } from '@angular/forms';
 import { ListadomatevaComponent } from './listadomateva/listadomateva.component';
 import { BoletinComponent } from './boletin/boletin.component';
@@ -27,19 +19,11 @@ import { MostrarboletinesComponent } from './mostrarboletines/mostrarboletines.c
 
 @NgModule({
   declarations: [
-    AltausuarioComponent,
-    EnviarnotificacionComponent,
-    ListadoaniosComponent,
+    EnviarnotificacionComponent,    
     ListadomateriasComponent,
-    NotasComponent,
     PaneladminComponent,
-    CrearevaluacionComponent,
-    NotaComponent,
-    CargarevaluacionComponent,
-    CargadenotasComponent,
-    ListadodivComponent,
-    ListadoalumnosComponent,
-    MateriasalumnoComponent,
+    CrearevaluacionComponent,  
+    ListadoalumnosComponent,    
     ListadomatevaComponent,
     BoletinComponent,
     MostrarboletinesComponent    
@@ -52,11 +36,8 @@ import { MostrarboletinesComponent } from './mostrarboletines/mostrarboletines.c
     
   ],
   exports:[
-    AltausuarioComponent,
-    EnviarnotificacionComponent,
-    ListadoaniosComponent,
-    ListadomateriasComponent,
-    NotasComponent,
+    EnviarnotificacionComponent,    
+    ListadomateriasComponent,    
     PaneladminComponent
 
   ],
--- proyectoEscuela-FRONT/src/app/pages/admin/admin.routes.ts
+++ proyectoEscuela-FRONT/src/app/pages/admin/admin.routes.ts
@@ -1,18 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { PaneladminComponent } from './paneladmin/paneladmin.component';
 import { LoginGuard } from 'src/app/guards/login.guard';
-import { AltausuarioComponent } from './altausuario/altausuario.component';
 import { EnviarnotificacionComponent } from './enviarnotificacion/enviarnotificacion.component';
-import { ListadoaniosComponent } from './listadoanios/listadoanios.component';
-import { NotasComponent } from './notas/notas.component';
 import { ListadomateriasComponent } from './listadomaterias/listadomaterias.component';
 import { CrearevaluacionComponent } from './crearevaluacion/crearevaluacion.component';
-import { NotaComponent } from './nota/nota.component';
-import { CargarevaluacionComponent } from './cargarevaluacion/cargarevaluacion.component';
-import { CargadenotasComponent } from './cargadenotas/cargadenotas.component';
-import { ListadodivComponent } from './listadodiv/listadodiv.component';
 import { ListadoalumnosComponent } from './listadoalumnos/listadoalumnos.component';
-import { MateriasalumnoComponent } from './materiasalumno/materiasalumno.component';
 import { RolAdmin } from 'src/app/guards/RolAdmin.guard';
 import { InicioComponent } from 'src/app/shared/inicio/inicio.component';
 import { ListadomatevaComponent } from './listadomateva/listadomateva.component';
@@ -25,18 +17,10 @@ const adminRoutes: Routes = [
     { path: 'paneladmin', component: PaneladminComponent,canActivate:[LoginGuard,RolAdmin],
         children: [
             { path: 'inicio', component: InicioComponent,canActivate:[LoginGuard]},
-            { path: 'altausuario', component: AltausuarioComponent,canActivate:[LoginGuard]},
             { path: 'enviaraviso', component: EnviarnotificacionComponent,canActivate:[LoginGuard,RolAdmin]},
-            { path: 'listadoanios', component: ListadoaniosComponent,canActivate:[LoginGuard]},
-            { path: 'notas', component: NotasComponent,canActivate:[LoginGuard,RolAdmin]},
             { path: 'listadomaterias', component: ListadomateriasComponent,canActivate:[LoginGuard,RolAdmin]},
             { path: 'crearevaluacion', component: CrearevaluacionComponent,canActivate:[LoginGuard,RolAdmin]},
-            { path: 'cargarevaluacion', component: CargarevaluacionComponent,canActivate:[LoginGuard,RolAdmin]},
-            { path: 'cargadenotas', component: CargadenotasComponent,canActivate:[LoginGuard,RolAdmin]},
-            { path: 'nota', component: NotaComponent,canActivate:[LoginGuard,RolAdmin]},
-            { path: 'divisiones', component: ListadodivComponent,canActivate:[LoginGuard,RolAdmin]},
             { path: 'listadoalumnos', component: ListadoalumnosComponent,canActivate:[LoginGuard,RolAdmin]},
-            { path: 'materiasalumno', component: MateriasalumnoComponent,canActivate:[LoginGuard,RolAdmin]},
             { path: 'materias', component: ListadomatevaComponent,canActivate:[LoginGuard,RolAdmin]},
             { path: 'cargarboletin', component: BoletinComponent,canActivate:[LoginGuard,RolAdmin]},
             { path: 'boletines', component: MostrarboletinesComponent,canActivate:[LoginGuard,RolAdmin]}
