df41434678e8fd243b4b19c6d287941053e0eb58
ivanchiabrera <44534420+ivanchiabrera@users.noreply.github.com>
2019-02-22 01:04:18 -0300

    .

--- proyectoEscuela-FRONT/src/app/pages/admin/listadomaterias/listadomaterias.component.ts
+++ proyectoEscuela-FRONT/src/app/pages/admin/listadomaterias/listadomaterias.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as CanvasJS from '../../../../../node_modules/canvasjs-2.3.1/canvasjs.min';
+
 
 @Component({
   selector: 'app-listadomaterias',
@@ -18,8 +20,22 @@ export class ListadomateriasComponent implements OnInit {
   evaluaciones:any;
   evaluacion:any;
   notas:any;
+  nota1=0;
+  nota2=0;
+  nota3=0;
+  nota4=0;
+  nota5=0;
+  nota6=0;
+  nota7=0;
+  nota8=0;
+  nota9=0;
+  nota10=0;
+
+
 
   ngOnInit() {
+
+    
 
     this.legajo=localStorage.getItem( 'legajo' );
 
@@ -58,10 +74,95 @@ export class ListadomateriasComponent implements OnInit {
   }
 
   cargarnotas(m:any){
+    this.nota1=0;this.nota2=0;this.nota3=0;this.nota4=0;this.nota5=0;this.nota6=0;this.nota7=0;this.nota8=0;this.nota9=0;this.nota10=0;
     this.http.get(`http://localhost:4000/evaluacion/display/${m.folio}`)
     .subscribe( data=> {
     this.evaluacion=data;
     this.notas=this.evaluacion.notas;
+    console.log(this.notas);
+
+    for (let index = 0; index < this.notas.length; index++) {
+
+      switch(this.notas[index].nota) { 
+        case 1: { 
+           this.nota1++; 
+           break; 
+        } 
+        case 2: { 
+          this.nota2++; 
+           break; 
+        }
+        case 3: { 
+          this.nota3++;
+          break; 
+       } 
+       case 4: { 
+        this.nota4++;
+        break; 
+       } 
+       case 5: { 
+        this.nota5++; 
+        break; 
+       } 
+       case 6: { 
+        this.nota6++;
+        break; 
+       } 
+       case 7: { 
+         console.log(7);
+        this.nota7++;
+        break; 
+       } 
+       case 8: { 
+        this.nota8++;
+       break; 
+       } 
+      case 9: { 
+        this.nota9++;
+       break; 
+       } 
+      case 10: { 
+        this.nota10++;
+      break; 
+       }   
+        default: { 
+           // statements; 
+           break; 
+        } 
+     } 
+      
+    }
+
+    const chart = new CanvasJS.Chart('chartContainer', {
+      theme: 'light1',
+      animationEnabled: true,
+      exportEnabled: true,
+      backgroundColor: '#E6E6E6',
+      title:{
+        text: 'Grafico'
+      },
+      data: [{
+        type: 'pie',
+        showInLegend: true,
+        toolTipContent: '<b>{name}</b>: Alumnos {y} (#percent%)',
+        indexLabel: '{name} - #percent%',
+        dataPoints: [
+          { y: this.nota1, name: 'Nota 1' },
+          { y: this.nota2, name: 'Nota 2' },
+          { y: this.nota3, name: 'Nota 3' },
+          { y: this.nota4, name: 'Nota 4' },
+          { y: this.nota5, name: 'Nota 5' },
+          { y: this.nota6, name: 'Nota 6'},
+          { y: this.nota7, name: 'Nota 7' },
+          { y: this.nota8, name: 'Nota 8'},
+          { y: this.nota9, name: 'Nota 9'},
+          { y: this.nota10, name: 'Nota 10'},
+
+        ]
+      }]
+    });
+      
+    chart.render();
     
     
     });
--- proyectoEscuela-FRONT/src/app/shared/prueba/prueba.component.ts
+++ proyectoEscuela-FRONT/src/app/shared/prueba/prueba.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as CanvasJS from '../../../../node_modules/canvasjs-2.3.1/canvasjs.min';
+
 
 @Component({
   selector: 'app-prueba',
@@ -26,10 +28,32 @@ export class PruebaComponent implements OnInit {
 
 
   ngOnInit() {
-    console.log(this.date);
-
-      return this.http.get('http://localhost:4000/colegio/alumno/materias/5275920')
-      .subscribe( data=> { this.prueba=data; });
+  
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Monthly Expense"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 1, name: "Comida" },
+          { y: 1, name: "Insurance" },
+          { y: 1, name: "Traveling" },
+          { y: 1, name: "Housing" },
+          { y: 1, name: "Education" },
+          { y: 1, name: "Shopping"},
+          { y: 2, name: "Others" }
+        ]
+      }]
+    });
+      
+    chart.render();
     
   }
 
